cc48c57e565f3b3195acca39dbf1cf60c9258884
0xFantasy <fcmfantasy1999@gmail.com>
2026-05-07 00:02:04 +0800

    chore: bump version to 1.3.1

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -7,8 +7,8 @@ object Configs {
 
     const val namespace = "com.fan.edgex"
     const val applicationId = "com.fan.edgex"
-    const val versionCode = 130
-    const val versionName = "1.3.0"
+    const val versionCode = 131
+    const val versionName = "1.3.1"
 
     val javaVersion = JavaVersion.VERSION_11
     const val jvmTarget = "11"
